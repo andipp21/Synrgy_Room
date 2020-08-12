97dad6544942b1be51bc51380f2772eb715c6e79
Andi Pratama Putra <andipp21@gmail.com>
2020-08-12 12:02:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/synrgy_room/AddActivity.kt
@@ -0,0 +1,39 @@
+package com.example.synrgy_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var db: DatabaseItem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+        DatabaseItem.getInstance(this)?.let {
+            db = it
+        }
+
+        btnSave.setOnClickListener {
+            val item = Item(null,etName.text.toString(), etQuantity.text.toString().toInt())
+
+            GlobalScope.launch {
+                val totalSaved = db.itemDao().addItem(item)
+
+                runOnUiThread{
+                    if (totalSaved>0){
+                        Toast.makeText(it.context, "Data telah disimpan", Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        Toast.makeText(this@AddActivity, "Data gagal disimpan", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/synrgy_room/ItemDao.kt
@@ -0,0 +1,20 @@
+package com.example.synrgy_room
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * FROM Item")
+    fun getAllItem(): List<Item>
+
+    @Insert(onConflict = REPLACE)
+    fun addItem(item: Item): Long
+
+    @Delete
+    fun deleteItem(item: Item): Int
+
+    @Update
+    fun updateItem(item: Item): Int
+}
